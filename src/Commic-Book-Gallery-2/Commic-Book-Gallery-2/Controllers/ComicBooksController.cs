541303136a571abd7633c7461c65d97217f40d7e
Israel Khachewatsky <izzykhach@gmail.com>
2020-06-24 21:43:45 -0700

    Added detail action method

--- src/Commic-Book-Gallery-2/Commic-Book-Gallery-2/Controllers/ComicBooksController.cs
+++ src/Commic-Book-Gallery-2/Commic-Book-Gallery-2/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace Commic_Book_Gallery_2.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
     }
 }
