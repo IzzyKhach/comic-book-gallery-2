c4f3c9514760cadd43c86447db88aa701e80d632
Israel Khachewatsky <izzykhach@gmail.com>
2020-06-24 21:01:50 -0700

    Initial commit

---
+++ src/Commic-Book-Gallery-2/Commic-Book-Gallery-2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Commic_Book_Gallery_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
