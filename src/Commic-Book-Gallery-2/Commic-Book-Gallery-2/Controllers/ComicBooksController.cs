ed355d1a65821a7857c0f94cb2f29bc42039123c
Israel Khachewatsky <izzykhach@gmail.com>
2020-06-26 17:50:57 -0700

    Using viewbag

--- src/Commic-Book-Gallery-2/Commic-Book-Gallery-2/Controllers/ComicBooksController.cs
+++ src/Commic-Book-Gallery-2/Commic-Book-Gallery-2/Controllers/ComicBooksController.cs
@@ -23,6 +23,20 @@ namespace Commic_Book_Gallery_2.Controllers
 
             //return Content("Hello from the comic books controller!");
 
+          
+
+            ViewBag.SeriesTitle = "The amazing Spider-Mann";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Fiinal issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
             return View();  //this goes to html view, prev does not
         }
     }
