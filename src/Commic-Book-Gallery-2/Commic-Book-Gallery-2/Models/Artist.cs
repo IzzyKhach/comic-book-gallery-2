3ccd180a621a5c63929404841b8a4889c498dba4
Israel Khachewatsky <izzykhach@gmail.com>
2020-06-28 13:25:07 -0700

    hgjhg

---
+++ src/Commic-Book-Gallery-2/Commic-Book-Gallery-2/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commic_Book_Gallery_2.Models
+{
+    public class Artist
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
--- src/Commic-Book-Gallery-2/Commic-Book-Gallery-2/Models/ComicBook.cs
+++ src/Commic-Book-Gallery-2/Commic-Book-Gallery-2/Models/ComicBook.cs
@@ -10,8 +10,8 @@ namespace Commic_Book_Gallery_2.Models
         public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
-        public string Description { get; set; }
-        public string[] Artists { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
     }
 }
